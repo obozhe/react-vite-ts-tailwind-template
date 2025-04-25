225ef36385d9293820d50fd7cb2acaaf4d8d33c6
Narek Avagian <obozhe@Nareks-MacBook-Pro.local>
2025-04-24 19:46:57 -0700

    tanstack added

--- data.ts
+++
@@ -1,7 +0,0 @@
-const contacts = [
-    { icon: faEnvelope, link: 'mailto:ava.narek@gmail.com' },
-    { icon: faPaperPlane, link: 'https://t.me/obozheee' },
-    { icon: faInstagram, link: 'https://www.instagram.com/nrk.vg/' },
-    { icon: faLinkedinIn, link: 'www.linkedin.com/in/narekavagian' },
-    { icon: faGithubAlt, link: 'https://github.com/obozhe' },
-];
